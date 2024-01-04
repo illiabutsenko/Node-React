9cb4c296ee73745fe9a3fa0c8f41a74b2ca4cc37
illiabutsenko <ilya1990.0826@gmail.com>
2024-01-04 16:31:42 +0100

    Node. Array

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Hello, World!");
+const _ = require('lodash');
+
+const myArray = [1, 2, 3, 4, 5];
+
+//In this example, _.tail function is used to obtain all elements of the array except the first one
+const elementsExceptFirst = _.tail(myArray);
+
+console.log(elementsExceptFirst);
