c19b0fb89d308079a690f01d6b80e303700a45ac
illiabutsenko <ilya1990.0826@gmail.com>
2024-01-06 13:23:38 +0100

    Node. CI/CD

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-if (module.hot) {
-  module.hot.accept();
-}
+// describe("Ура! Наш первый автотест", function () {
+//   it("Тест 1.1", function () {
+//     expect(1 + 1).toBe(2);
+//   });
+// });
