c9e7a321ff3ac68be673bbad9552fec5b1f84c29
illiabutsenko <ilya1990.0826@gmail.com>
2024-01-16 20:54:42 +0100

    React. Single-page application by React Router for my components

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,37 @@
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import Counter from "./components/Counter";
+import Joke from "./components/Joke";
+import Posts from "./components/Posts";
+import Toolbar from "./components/Toolbar";
 
 function App() {
   return (
     <div>
-      <Counter />
+      <BrowserRouter>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/counter">Counter</Link>
+            </li>
+            <li>
+              <Link to="/joke">Joke</Link>
+            </li>
+            <li>
+              <Link to="/posts">Posts</Link>
+            </li>
+            <li>
+              <Link to="/toolbar">Toolbar</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/counter" element={<Counter />} />
+          <Route path="/joke" element={<Joke />} />
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/toolbar" element={<Toolbar />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
