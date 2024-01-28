fa7a07ce465408b9c8051b1e25fe043f0d72d746
illiabutsenko <ilya1990.0826@gmail.com>
2024-01-28 19:06:33 +0100

    CSS-in-JS

---
+++ src/components/Button.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: green;
+  color: white;
+
+  &:hover {
+    background-color: blue;
+  }
+`;
+
+export default Button;
