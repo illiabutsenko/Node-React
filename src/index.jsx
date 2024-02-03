7bd7cdb3cfca08b29401fa6fa6261b66e6d40f48
illiabutsenko <ilya1990.0826@gmail.com>
2024-02-03 16:46:51 +0100

    React. Tailwind themes

--- src/index.jsx
+++ src/index.jsx
@@ -2,11 +2,20 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
 import CounterOld from "./components/CounterOld";
 import Posts from "./components/Posts";
-import Toolbar from "./components/Toolbar";
+import Recipes from "./components/Recipes";
+import Switch from "react-switch";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [isDark, setIsDark] = useState(true);
+
+  useEffect(() => {
+    if (isDark) {
+      document.body.classList.add("dark");
+    } else document.body.classList.remove("dark");
+  }, [isDark]);
   return (
-    <div className="container mx-auto px-4">
+    <div className="container mx-auto px-4 dark:bg-gray-900 dark:text-white">
       <BrowserRouter>
         <nav className="bg-gray-800 rounded-lg p-4">
           <ul className="text-white flex flex-wrap justify-center space-x-4">
@@ -21,16 +30,26 @@ function App() {
               </Link>
             </li>
             <li>
-              <Link className="hover:underline" to="/toolbar">
-                Toolbar
+              <Link className="hover:underline" to="/Recipes">
+                Recipes
               </Link>
+            </li>
+            <li>
+              <Switch
+                uncheckedIcon="🌙"
+                checkedIcon="☀️"
+                width={46}
+                height={24}
+                checked={isDark}
+                onChange={(toggle) => setIsDark(toggle)}
+              />
             </li>
           </ul>
         </nav>
         <Routes>
           <Route path="/counter" element={<CounterOld />} />
           <Route path="/posts" element={<Posts />} />
-          <Route path="/toolbar" element={<Toolbar />} />
+          <Route path="/Recipes" element={<Recipes />} />
         </Routes>
         <h1 class="bg-indigo-300 flex justify-center">Header</h1>
         <p className="bg-green-300 text-sky">Paragraph</p>
